4df07cadf9b3f172bbe74bb107cbb905433a0d1a
sawadym-stack <sawadympes@gmail.com>
2025-10-21 21:58:22 +0530

    Time: 53 ms (59.98%) | Memory: 57 MB (75.99%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+    
+  let result = [];
+
+  for (let i = 0; i < n; i++) {
+    result.push(nums[i])    
+    result.push(nums[i + n])
+// console.log(i)
+// console.log(n)
+
+  }
+
+  return result;
+
+
+};
