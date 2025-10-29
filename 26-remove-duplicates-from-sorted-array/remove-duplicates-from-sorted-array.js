97b2a60e19ee919e3c098d5b44de227500c92d7f
sawadym-stack <sawadympes@gmail.com>
2025-10-29 13:34:53 +0530

    Time: 0 ms (100.00%) | Memory: 56.6 MB (89.51%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+if (nums.length === 0) return 0;
+let j =0;
+   for(let i = 0;i<nums.length;i++){
+    if(nums[j] !== nums[i]){
+        j++;
+        nums[j]=nums[i]
+    }
+   }
+   return j+1
+};
