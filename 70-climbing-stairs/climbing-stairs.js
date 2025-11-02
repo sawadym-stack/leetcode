44d33eee4aee07b33de9da375a557d1eff4f0e25
sawadym-stack <sawadympes@gmail.com>
2025-11-02 14:26:42 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (59.30%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if (n <= 2) return n;
+
+  let first = 1; 
+  let second = 2;
+  let total;
+
+  for (let i = 3; i <= n; i++) {
+    total = first + second; 
+    first = second;
+    second = total;
+  }
+
+  return total;
+};
