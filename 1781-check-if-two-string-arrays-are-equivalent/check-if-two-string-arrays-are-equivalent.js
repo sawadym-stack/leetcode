10c5b90f23248e3d5a50ce36fb35683fe133ad74
sawadym-stack <sawadympes@gmail.com>
2025-11-04 19:40:16 +0530

    Time: 0 ms (100.00%) | Memory: 54.1 MB (78.45%) - LeetSync

---
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+var arrayStringsAreEqual = function(word1, word2) {
+    // for(let i = 0;i<word1.length;i++){
+        
+    // }
+    let arr= word1.join("")
+    let arr1= word2.join("")
+    if(arr==arr1){
+        return true
+    }else{
+        return false
+    }
+    // console.log(arr)
+};
