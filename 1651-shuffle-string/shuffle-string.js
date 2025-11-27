aaeb1b3265fede6487be90bad33190513e45387b
sawadym-stack <sawadympes@gmail.com>
2025-11-27 21:11:50 +0530

    Time: 1 ms (76.58%) | Memory: 54.9 MB (88.61%) - LeetSync

---
+++ 1651-shuffle-string/shuffle-string.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function(s, indices) {
+     let res = Array(s.length);
+
+    for (let i = 0; i < s.length; i++) {
+        res[indices[i]] = s[i];
+    }
+
+    return res.join('');
+};
