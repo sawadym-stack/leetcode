5a94df4a8277efd0d1f1daba7f54a03700b3683e
sawadym-stack <sawadympes@gmail.com>
2026-02-16 09:49:55 +0530

    Time: 0 ms (100.00%) | Memory: 6.8 MB (33.55%) - LeetSync

---
+++ 203-remove-linked-list-elements/remove-linked-list-elements.go
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeElements(head *ListNode, val int) *ListNode {
+    dummy:=&ListNode{Next:head}
+    current:=dummy
+
+    for current.Next != nil {
+        if current.Next.Val==val{
+            current.Next = current.Next.Next
+        }else{
+            current= current.Next
+        }
+
+    }
+       return dummy.Next
+}
