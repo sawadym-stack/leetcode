884adfd4ceb656033a3cb4c4be005f2cdd8afa3a
sawadym-stack <sawadympes@gmail.com>
2026-04-02 14:32:51 +0530

    Time: 0 ms (100.00%) | Memory: 4 MB (45.63%) - LeetSync

--- 75-sort-colors/sort-colors.go
+++ 75-sort-colors/sort-colors.go
@@ -1,13 +1,10 @@
 func sortColors(nums []int)  {
-    n:=len(nums)
+  n:=len(nums)
 	for i:=0;i<n;i++{
-		min:=i
-		for j:=i+1;j<n;j++{
-           if nums[j]<nums[min]{
-			min = j
-		   }
+       for j:=0;j<n-i-1;j++{
+		if nums[j]>nums[j+1]{
+			nums[j],nums[j+1]=nums[j+1],nums[j]
 		}
-		nums[i],nums[min]=nums[min],nums[i]
+	   }
 	}
-	
 }
