ac33b5b3b9e8e0ff6e6e85ac299db96b1e414fd0
sawadym-stack <sawadympes@gmail.com>
2026-04-03 11:06:51 +0530

    Time: 4 ms (51.65%) | Memory: 5.1 MB (14.36%) - LeetSync

--- 242-valid-anagram/valid-anagram.go
+++ 242-valid-anagram/valid-anagram.go
@@ -1,13 +1,20 @@
 func isAnagram(s string, t string) bool {
+    // if len(s) != len(t) { return false }
+
+	// m := map[byte]int{}
+	// for i := range s {
+	// 	m[s[i]]++
+	// 	m[t[i]]--
+	// }
+	// for _, v := range m {
+	// 	if v != 0 { return false }
+	// }
+	// return true
     if len(s) != len(t) { return false }
 
-	m := map[byte]int{}
-	for i := range s {
-		m[s[i]]++
-		m[t[i]]--
-	}
-	for _, v := range m {
-		if v != 0 { return false }
-	}
-	return true
+	a, b := []byte(s), []byte(t)
+	sort.Slice(a, func(i, j int) bool { return a[i] < a[j] })
+	sort.Slice(b, func(i, j int) bool { return b[i] < b[j] })
+
+	return string(a) == string(b)
 }
