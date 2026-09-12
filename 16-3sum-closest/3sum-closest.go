d637aaac6429a8254d9ddfe4a374167215b309d2
sawadym-stack <sawadympes@gmail.com>
2026-09-12 10:49:09 +0530

    Time: 4 ms (82.59%) | Memory: 4.7 MB (83.79%) - LeetSync

---
+++ 16-3sum-closest/3sum-closest.go
@@ -0,0 +1,36 @@
+func threeSumClosest(nums []int, target int) int {
+    	sort.Ints(nums)
+	n := len(nums)
+
+	closest := nums[0] + nums[1] + nums[2]
+
+	for i := 0; i < n-2; i++ {
+		left, right := i+1, n-1
+
+		for left < right {
+			sum := nums[i] + nums[left] + nums[right]
+
+			// update closest
+			if abs(sum-target) < abs(closest-target) {
+				closest = sum
+			}
+
+			if sum < target {
+				left++
+			} else if sum > target {
+				right--
+			} else {
+				return sum // exact match
+			}
+		}
+	}
+
+	return closest
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
